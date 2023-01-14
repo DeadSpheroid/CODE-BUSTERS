4e054a193298e5fc94d7b88c8c7b6f42e637e9bf
DeadSpheroid <warrenjacinto@gmail.com>
2023-01-14 09:22:59 +0530

    final commit

---
+++ webapp/public/script.js
@@ -0,0 +1,6 @@
+function handleSubmit(e)
+{
+    e.preventDefault()
+
+    console.log("helllo")
+}
--- webapp/server.js
+++ webapp/server.js
@@ -1,26 +1,28 @@
 const http = require('http');
 const formidable = require('formidable');
-const mg= require("mongodb");
-const fs= require("fs");
-
+const mg = require("mongodb");
+const fs = require("fs");
+let email;
+let pass;
 // Import the functions you need from the SDKs you need
 
-const fbapp=require("firebase/app")
+const fbapp = require("firebase/app")
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBYRufixyjzqxXxEjguwMI-6GmLt05s7x0",
-  authDomain: "codebusters-190dd.firebaseapp.com",
-  projectId: "codebusters-190dd",
-  storageBucket: "codebusters-190dd.appspot.com",
-  messagingSenderId: "771971235354",
-  appId: "1:771971235354:web:bea25b3d3cdf20b96adba3",
-  measurementId: "G-MHQ5V5C2W3",
-  databaseURL: "https://codebusters-190dd-default-rtdb.asia-southeast1.firebasedatabase.app/"
+  apiKey: "AIzaSyBqAq9n8Jis-W2fyO5JgfH610zFubmY-AU",
+  authDomain: "scholarship-portal-5bd6b.firebaseapp.com",
+  databaseURL: "https://scholarship-portal-5bd6b-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "scholarship-portal-5bd6b",
+  storageBucket: "scholarship-portal-5bd6b.appspot.com",
+  messagingSenderId: "491056912929",
+  appId: "1:491056912929:web:c7fa5ba183c1ec5928e160",
+  measurementId: "G-L7SZBMMN0Y"
 };
+
 
 // Initialize Firebase
 const app = fbapp.initializeApp(firebaseConfig);
@@ -31,42 +33,56 @@ const fbdb = require("firebase/database");
 
 // Initialize Realtime Database and get a reference to the service
 const database = fbdb.getDatabase(app);
-const dbref =fbdb.ref(database);
-http.createServer((req,res)=>{
+const dbref = fbdb.ref(database);
 
+const express = require('express');
+const bodyParser = require("body-parser");
+const expapp = express();
+const multer = require('multer');
+expapp.use(express.static('public'));
+//const { getAuth, signInWithEmailAndPassword } = require('firebase/auth');
+const upload = multer();
+const port = 3000;
+const fbau = require("firebase/auth");
+const { Console } = require('console');
+//expapp.use(express.static('public'));
+expapp.use(bodyParser.urlencoded({ extended: true }));
+expapp.get('/', (req, res) => {
+  res.sendFile(__dirname + "/public/login.html");
+})
 
-    console.log("Server Running");
-    
-    if (req.url="/loginpage")
-    {   
-        console.log("Here");
-        res.writeHead(200, {'Content-Type':'text/html'});
-        fs.readFile("webapp\\login.html", (err,data)=>{
-            if(err) throw err;
-            res.write(data);
-            return res.end();
-        })
-        fbdb.get(fbdb.child(dbref, `Users`)).then((snapshot) => {
-            if (snapshot.exists()) {
-              console.log(snapshot.val());
-            } else {
-              console.log("No data available");
-            }
-          }).catch((error) => {
-            console.error(error);
-          });
-        function FormParse(){
+expapp.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
 
-          let form= new formidable.IncomingForm();
-          console.log("promised");
-          prom.then(()=>{
-            form.parse(req,(err,fields,files)=>{
-              console.log(fields);
-              console.log("then");
-            })
-          })
-        }
+const auth = fbau.getAuth(app);
+// for parsing multipart/form-data
+expapp.use(upload.array());
+expapp.post("/", (req, res) => {
+  email = req.body.Email;
+  pass = req.body.Password;
+  console.log(email + " " + pass);
+  expapp.route("/processing/");
+  console.log("Routed");
+})
 
-    }
-}).listen(5500)
-//http://localhost:5500/loginpage/
+expapp.get("/processing/", (req, res) => {
+  console.log("Processing");
+  res.sendFile(__dirname + "/processing.html");
+  fbau.signInWithEmailAndPassword(auth, email, pass)
+    .then((userCredentials) => {
+      const user = userCredentials.user;
+      console.log("THENED");
+      expapp.route("/home/");
+    })
+    .catch((error) => {
+      if (error) Console.log(error);
+      expapp.route("/")
+    })
+  //if successfull login route to /home, if not route to /
+})
+
+expapp.get("/home/", (req, res) => {
+  res.sendFile(__dirname + "/mainpage.html");
+})
+//http://localhost:3000/
